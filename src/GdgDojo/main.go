abb578d97413191dd939df2e863e230708510e87
m4rco0 <gamermarco28@gmail.com>
2024-10-04 00:32:02 -0300

    Buscar produto e printar no log

--- src/GdgDojo/main.go
+++ src/GdgDojo/main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
+	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"go.uber.org/zap"
 )
 
 func main() {
-	dsn := "host=localhost user=postgres password=default dbname=postgres port=5432 sslmode=disable"
+	// configurando o logger
 	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+
+	// conectando db
+	dsn := "host=localhost user=postgres password=default dbname=postgres port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		logger.Error("Erro em se conectar")
+		logger.Fatal("Falha em connectar com o banco de dados", zap.Error(err))
 	}
-	db.AutoMigrate(&Estoque{})
-	createProduct(db, logger, 50,"sabato",150.0)
+	if err := db.AutoMigrate(&Estoque{}); err != nil  {
+		logger.Fatal("Falha em migrar o banco de dados", zap.Error(err))
+	} 
 
-
+	//createProduct(db, logger, 50,"sabato",150.0)
+	findID(db,logger, 5)
 }
 
 func createProduct (db *gorm.DB, logger *zap.Logger, quantidade int, product string, price float64) {
@@ -29,8 +35,17 @@ func createProduct (db *gorm.DB, logger *zap.Logger, quantidade int, product str
 	}
 }
 
-func searchProduct (db *gorm.DB, logger *zap.Logger, id uint) {
-	
+// função para buscar o id de um produto
+func findID(db *gorm.DB, logger *zap.Logger, id uint) {
+	var produto Estoque
+
+	res := db.First(&produto, id)
+	if(res.Error != nil) {
+		logger.Fatal("Falha ao procurar produto", zap.Uint("ID", id), zap.Error(res.Error))
+		return
+	}
+	logger.Info("Produto encontrado", zap.Uint("ID", produto.ID), zap.String("Product",produto.Product))
+	//fmt.Printf("Produto ID: %d\n", produto.ID)
 }
 
 type Estoque struct {
